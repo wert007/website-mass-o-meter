e7153983551dea8f3a6afe90a85016ba6b87d8e0
ranplax <niklasleukroth@gmail.com>
2020-05-10 16:33:51 +0200

    sdf

--- gallery.js
+++ gallery.js
@@ -1,3 +1,70 @@
+function dunno(gender, mode)
+{
+    let prev, cur, n;
+    if(gender == 'damen')
+    {
+        prev = "damenLeft";
+        cur = "damenCenter";
+        n = "damenRight";
+    }
+    else if(gender == 'herren')
+    {
+        prev = "herrenLeft";
+        cur = "herrenCenter";
+        n = "herrenRight";
+    }    
+    
+    let previous = document.getElementById(prev).src.toString();
+    let current = document.getElementById(cur).src.toString();
+    let next = document.getElementById(n).src.toString();
+    let picLeft, picCenter, picRight;
+
+    if(mode == 'left')
+    {
+        picLeft = shiftLeft(gender, getPicture(previous));
+        picCenter = shiftLeft(gender, getPicture(current));
+        picRight = shiftLeft(gender, getPicture(next));
+    }
+    else if(mode == 'right')
+    {
+        picLeft = shiftRight(gender, getPicture(previous));
+        picCenter = shiftRight(gender, getPicture(current));
+        picRight = shiftRight(gender, getPicture(next));
+    }
+    
+    document.getElementById(prev).src = "./assets/bilder/" + gender + "/" + picLeft + ".jpg";
+    document.getElementById(cur).src = "./assets/bilder/" + gender + "/" + picCenter + ".jpg";
+    document.getElementById(n).src = "./assets/bilder/" + gender + "/" + picRight + ".jpg";
+}
+
+function getPicture(src)
+{
+    src = src.replace ( /[^\d]/g, '' );
+    let num = parseInt(src);
+    num = num > 800000 ? num - 808000 : num - 80800;
+    return num;
+}
+
+function shiftLeft(gender, pic)
+{
+   let mod = gender == "damen" ? 20 : 8;
+   console.log(pic);
+   pic = pic <= 1 ? (mod + --pic)%mod : --pic;
+   if(pic == 0) pic = mod;
+   //console.log(pic);
+   return pic.toString();
+}
+
+function shiftRight(gender, pic)
+{ 
+    let mod = gender == "damen" ? 20 : 8;
+    pic = ++pic % mod;
+    if(pic == 0) pic++;
+
+    return pic.toString();
+}
+
+/*
 let indices = {}
 
 function load(galleryId) {
@@ -35,4 +102,4 @@ function setFocus(parent, path, index) {
             img.className = "gallery previewRight";
 
     }
-}
+}*/
