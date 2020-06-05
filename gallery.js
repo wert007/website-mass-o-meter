81b299500c839b18cebe09221be8dd793186c882
ranplax <niklasleukroth@gmail.com>
2020-06-05 14:20:44 +0200

    j

--- gallery.js
+++ gallery.js
@@ -3,7 +3,7 @@ function slideshow(gender, idNumber)
     imageId = "" + gender + idNumber;
     imageId = imageId.toString();
     className = document.getElementById(imageId).className.toString();
-    console.log(className);
+
     if(className == "imageCenter") return;
 
     if(className == "imageLeft") shiftLeft(gender, imageId, idNumber);
@@ -12,16 +12,7 @@ function slideshow(gender, idNumber)
 }
 
 function shiftRight(gender, centerId, idNumber)
-{/*
-    let mod = gender == "damen" ? 20 : 8;
-
-    let left = (idNumber - 1) < 0 ? mod : idNumber - 1; 
-    let right = (idNumber + 1)
-    let notVisible = (idNumber - 2) < 0 ? mod - 1 + idNumber : idNumber - 2;
-    if(gender == "damen") right = idNumber == 19 ? 0 : right;
-    if(gender == "herren") right = idNumber == 7 ? 0 : right; 
-
-    showSlides(gender, notVisible, left, centerId, right);*/
+{
     let mod = gender == "damen" ? 19 : 7;
 
     let left = idNumber - 1;
@@ -64,6 +55,4 @@ function showSlides(gender, notVisible, left, centerId, right)
     document.getElementById(leftId).className = "imageLeft";
     document.getElementById(centerId).className = "imageCenter";
     document.getElementById(rightId).className = "imageRight";
-
 }
-
