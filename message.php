48dcdd238cf34e7a86a2bfad6a96847fcfcc98b0
ranplax <niklasleukroth@gmail.com>
2020-07-18 16:30:05 +0200

    now

--- message.php
+++ message.php
@@ -2,10 +2,10 @@
     if(isset($_POST['submit'])){
         $to = "niklasleukroth@gmail.com"; // this is your Email address
         $from = "niklasleukroth@gmail.com"; // this is the sender's Email address
-        $name = $_POST['last_name'];
-        $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+        $name = $_POST['name'];
+        $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
 
-        mail($to,$subject,$message,$headers);
+        mail($to,$message);
         echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
         }
 ?>
