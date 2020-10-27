c9c534818019bd3ea2742ea907b7898d36ad1b5e
wert007 <pablohamacher@web.de>
2020-10-27 16:11:45 +0100

    wovon sprichst du!?

--- index.php
+++ index.php
@@ -246,7 +246,7 @@
             // $headers .= "BCC: hidden@example.com\r\n";
 
             
-            $headers .= "Organization: Sender Organization\r\n";
+            // $headers .= "Organization: Sender Organization\r\n";
             $headers .= "MIME-Version: 1.0\r\n";
             $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
             $headers .= "X-Priority: 3\r\n";
