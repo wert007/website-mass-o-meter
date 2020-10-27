638ef3eb0aa193b05643e8f959a539e62ce1fb2f
wert007 <pablohamacher@web.de>
2020-10-27 18:46:11 +0100

    just testing it out :)

--- gallery.js
+++ gallery.js
@@ -2,23 +2,32 @@ const femalePictures = 19;
 const malePictures = 7;
 const colorPictures = 5;
 
-function slideshow(gender, idNumber)
-{
+function slideshow(gender, idNumber) {
     imageId = "" + gender + idNumber;
     imageId = imageId.toString();
     className = document.getElementById(imageId).className.toString();
 
-    if(className == "imageCenter") return;
+    if (className == "imageCenter") makeBig(gender, imageId, idNumber);
+    else if (className == "imageBig") makeCenter(gender, imageId, idNumber);
 
-    if(className == "imageLeft") shiftLeft(gender, imageId, idNumber);
-    else if(className == "imageRight") shiftRight(gender, imageId, idNumber);
-    return;
+    if (className == "imageLeft") shiftLeft(gender, imageId, idNumber);
+    else if (className == "imageRight") shiftRight(gender, imageId, idNumber);
 }
 
-function shiftRight(gender, centerId, idNumber)
-{
+
+function makeBig(gender, centerId, idNumber) {
+    document.getElementById(gender + centerId).className = 'imageBig';
+    document.getElementById('blackscreen').style = 'display: visible;';
+}
+
+function makeSmall(gender, centerId, idNumber) {
+    document.getElementById(gender + centerId).className = 'imageCenter';
+    document.getElementById('blackscreen').style = 'display: hidden;';
+}
+
+function shiftRight(gender, centerId, idNumber) {
     let mod = gender == "damen" ? femalePictures : malePictures;
-    if(gender == "color") mod = colorPictures;
+    if (gender == "color") mod = colorPictures;
 
     let left = (idNumber - 1 + mod) % mod;
     let right = (idNumber + 1 + mod) % mod;
@@ -29,10 +38,9 @@ function shiftRight(gender, centerId, idNumber)
 }
 
 //Clicked on the left pic
-function shiftLeft(gender, centerId, idNumber)
-{
+function shiftLeft(gender, centerId, idNumber) {
     let mod = gender == "damen" ? femalePictures : malePictures;
-    if(gender == "color") mod = colorPictures;
+    if (gender == "color") mod = colorPictures;
 
     let left = (idNumber - 1 + mod) % mod;
     let right = (idNumber + 1 + mod) % mod;
@@ -41,12 +49,11 @@ function shiftLeft(gender, centerId, idNumber)
     showSlides(gender, notVisible, left, centerId, right);
 }
 
-function showSlides(gender, notVisible, left, centerId, right)
-{
+function showSlides(gender, notVisible, left, centerId, right) {
     let notVisibleId = ("" + gender + notVisible).toString();
     let leftId = ("" + gender + left).toString();
     let rightId = ("" + gender + right).toString();
-    
+
 
     document.getElementById(notVisibleId).className = "imageHidden";
     document.getElementById(leftId).className = "imageLeft";
